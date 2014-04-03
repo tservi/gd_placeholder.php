ec93faef2f415c88fb4eeb84700307add17de241
Jean Tinguely Awais aka t-servi.com <jeantinguelyawais@gmail.com>
2014-04-03 17:53:19 +0200

    Ajout de nouveaux paramètres
    Nouveaux paramètres pour la couleur du texte...

--- gd_placeholder.php
+++ gd_placeholder.php
@@ -6,20 +6,28 @@
  * like http://placehold.it/ or http://placekitten.com/
  */
 
-$text = "Placeholder";
+ 
+ 
+$text = "GD Placeholder by t-servi.com";
 $width = 110;
 $height = 20;
-if( isset( $_REQUEST[ 'text' ])) {$text = $_GET['text'];}
-if( isset( $_REQUEST[ 'width' ])) {$text = $_GET['width'];}
-if( isset( $_REQUEST[ 'height' ])) {$text = $_GET['height'];}
+$red = 220;
+$green = 210;
+$blue = 60;
+if( isset( $_REQUEST[ 'text' ])) {$text = $_REQUEST['text'];}
+if( isset( $_REQUEST[ 'width' ])) {$width = $_REQUEST['width'];}
+if( isset( $_REQUEST[ 'height' ])) {$height = $_REQUEST['height'];}
+if( isset( $_REQUEST[ 'red' ])) {$red = $_REQUEST['red'];}
+if( isset( $_REQUEST[ 'green' ])) {$green = $_REQUEST['green'];}
+if( isset( $_REQUEST[ 'blue' ])) {$red = $_REQUEST['blue'];}
 
 header("Content-type: image/png");
 $im = @imagecreatetruecolor($width, $height)
-    or die("Impossible d'initialiser la bibliothèque GD");
+    or die("GD Library is not present!");
 
-$orange = imagecolorallocate($im, 220, 210, 60);
+$text_color = imagecolorallocate($im, $red, $green, $blue);
 $px     = (imagesx($im) - 7.5 * strlen($text)) / 2;
-imagestring($im, 3, $px, 9, $text, $orange);
+imagestring($im, 3, $px, 9, $text, $text_color);
 imagepng($im);
 imagedestroy($im);
 
